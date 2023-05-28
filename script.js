b75e62b80e621926b0fd835bf7f8a44601ed29d8
sanavalos <santiago.avalos@outlook.com>
2023-05-28 20:56:01 -0300

    feat(html, script): current time

--- script.js
+++ script.js
@@ -22,18 +22,20 @@ input.addEventListener("keyup", function (e) {
 var firstSender = true;
 var firstRecieved = true;
 
-let hourMinutes = new Date().toLocaleTimeString("en-US", {
-  hour: "numeric",
-  minute: "numeric",
-});
+function getTime() {
+  let hourMinutes = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+  });
 
-hourMinutes =
-  hourMinutes.slice(0, hourMinutes.length - 1) +
-  "." +
-  hourMinutes.slice(hourMinutes.length - 1) +
-  ".";
+  hourMinutes =
+    hourMinutes.slice(0, hourMinutes.length - 1) +
+    "." +
+    hourMinutes.slice(hourMinutes.length - 1) +
+    ".";
 
-hourMinutes = hourMinutes.toLowerCase();
+  return hourMinutes.toLowerCase();
+}
 
 function updateValue() {
   if (input.value.startsWith("NAME:"))
@@ -47,24 +49,24 @@ function updateValue() {
   else if (firstSender && input.value.startsWith("RIGHT:")) {
     chatContainer.innerHTML += `<div class='message sent-first'>${input.value.slice(
       6
-    )}<span class='metadata'><span class='time'>${hourMinutes}</span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span>
+    )}<span class='metadata'><span class='time'>${getTime()}</span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span>
     </span>
   </div>`;
     firstSender = false;
   } else if (!firstSender && input.value.startsWith("RIGHT:")) {
     chatContainer.innerHTML += `<div class="message sent-after">${input.value.slice(
       6
-    )}<span class="metadata"><span class="time">${hourMinutes}</span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span> </span></div>`;
+    )}<span class="metadata"><span class="time">${getTime()}</span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span> </span></div>`;
     firstRecieved = true;
   } else if (firstRecieved && input.value.startsWith("LEFT:")) {
     chatContainer.innerHTML += `<div class="message received-first">${input.value.slice(
       5
-    )}<span class="metadata"><span class="time">${hourMinutes}</span></span></div>`;
+    )}<span class="metadata"><span class="time">${getTime()}</span></span></div>`;
     firstRecieved = false;
   } else if (!firstRecieved && input.value.startsWith("LEFT:")) {
     chatContainer.innerHTML += `<div class="message received-after">${input.value.slice(
       5
-    )}<span class="metadata"><span class="time">${hourMinutes}</span></span></div>`;
+    )}<span class="metadata"><span class="time">${getTime()}</span></span></div>`;
   }
   msgMic.classList.remove("zmdi-mail-send");
   msgMic.classList.add("zmdi-mic");
